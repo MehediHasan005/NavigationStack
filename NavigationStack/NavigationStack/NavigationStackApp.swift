3f96f71bd63d74e9aa445e8c6b59feef7ddf5d17
MehediHasan005 <52360118+MehediHasan005@users.noreply.github.com>
2022-11-27 00:03:58 +0600

    Add files via upload
    Finished

---
+++ NavigationStack/NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Mehedi Hasan on 26/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
